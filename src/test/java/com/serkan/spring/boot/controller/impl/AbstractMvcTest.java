42f307de28b2e465b19a51320f745b7dfa1e3bd8
Serkan Kaba <serkan_kaba@yahoo.com>
2015-04-09 14:03:09 +0200

    Assert Full json content type.

--- src/test/java/com/serkan/spring/boot/controller/impl/AbstractMvcTest.java
+++ src/test/java/com/serkan/spring/boot/controller/impl/AbstractMvcTest.java
@@ -4,12 +4,17 @@
  */
 package com.serkan.spring.boot.controller.impl;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+import java.nio.charset.Charset;
+
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.EmbeddedWebApplicationContext;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,6 +27,8 @@ import com.serkan.spring.boot.BootApplication;
 @WebAppConfiguration
 @IntegrationTest({ "server.port=8080" })
 public abstract class AbstractMvcTest<T> {
+
+    public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(APPLICATION_JSON.getType(), APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
 
     @Autowired
     private EmbeddedWebApplicationContext webApplicationContext;
--- src/test/java/com/serkan/spring/boot/controller/impl/MathOperationControllerMvcTest.java
+++ src/test/java/com/serkan/spring/boot/controller/impl/MathOperationControllerMvcTest.java
@@ -4,7 +4,6 @@
  */
 package com.serkan.spring.boot.controller.impl;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -16,27 +15,27 @@ public class MathOperationControllerMvcTest extends AbstractMvcTest<MathOperatio
 
     @Test
     public void testAdd() throws Exception {
-        getMockMvc().perform(get("/math/add").param("n", "0", "1")).andExpect(status().isOk()).andExpect(content().string("1")).andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON));
+        getMockMvc().perform(get("/math/add").param("n", "0", "1")).andExpect(status().isOk()).andExpect(content().string("1")).andExpect(content().contentType(APPLICATION_JSON_UTF8));
     }
 
     @Test
     public void testSubtract() throws Exception {
-        getMockMvc().perform(get("/math/subtract").param("n", "0", "1")).andExpect(status().isOk()).andExpect(content().string("-1")).andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON));
+        getMockMvc().perform(get("/math/subtract").param("n", "0", "1")).andExpect(status().isOk()).andExpect(content().string("-1")).andExpect(content().contentType(APPLICATION_JSON_UTF8));
     }
 
     @Test
     public void testDivide() throws Exception {
-        getMockMvc().perform(get("/math/divide").param("n", "8", "2")).andExpect(status().isOk()).andExpect(content().string("4")).andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON));
+        getMockMvc().perform(get("/math/divide").param("n", "8", "2")).andExpect(status().isOk()).andExpect(content().string("4")).andExpect(content().contentType(APPLICATION_JSON_UTF8));
     }
 
     @Test
     public void testMultiply() throws Exception {
-        getMockMvc().perform(get("/math/multiply").param("n", "8", "2")).andExpect(status().isOk()).andExpect(content().string("16")).andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON));
+        getMockMvc().perform(get("/math/multiply").param("n", "8", "2")).andExpect(status().isOk()).andExpect(content().string("16")).andExpect(content().contentType(APPLICATION_JSON_UTF8));
     }
 
     @Test
     public void testAverage() throws Exception {
-        getMockMvc().perform(get("/math/average").param("n", "8", "2")).andExpect(status().isOk()).andExpect(content().string("5.0")).andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON));
+        getMockMvc().perform(get("/math/average").param("n", "8", "2")).andExpect(status().isOk()).andExpect(content().string("5.0")).andExpect(content().contentType(APPLICATION_JSON_UTF8));
     }
 
     @Test
